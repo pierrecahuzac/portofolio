3d50f5246a1d9bd6c40232b528d491ba29abe0ff
pierrecahuzac <cahuzac.p@gmail.com>
2025-05-12 18:13:58 +0200

    :fix: fix project and SCSS

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/',
+  server: {
+    host: '0.0.0.0'
+
+  },
   build: {
     outDir: 'dist',
     assetsDir: 'assets'
