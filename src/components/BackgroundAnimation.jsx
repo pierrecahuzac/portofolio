78f94f68c2c885c788d1470a7a897ca9c0137712
pierrecahuzac <cahuzac.p@gmail.com>
2025-05-12 13:09:02 +0200

    :update: update background particles

--- src/components/BackgroundAnimation.jsx
+++ src/components/BackgroundAnimation.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useMemo, useState } from "react";
 import { initParticlesEngine, Particles } from "@tsparticles/react";
 import "../assets/styles/BackgroundAnimation.scss";
-import { loadSlim,  } from "@tsparticles/slim";
+import { loadSlim, } from "@tsparticles/slim";
 const BackgroundAnimation = () => {
   const [init, setInit] = useState(false);
   const particlesLoaded = (container) => {
@@ -11,55 +11,38 @@ const BackgroundAnimation = () => {
     () => ({
       background: {
         color: {
-          value: "#0d47a1",
+          value: "#223a55",
         },
       },
       fpsLimit: 120,
-      interactivity: {
-        events: {
-          onClick: {
-            enable: true,
-            mode: "push",
-          },
-          onHover: {
-            enable: true,
-            mode: "repulse",
-          },
-        },
-        modes: {
-          push: {
-            quantity: 4,
-          },
-          repulse: {
-            distance: 200,
-            duration: 0.4,
-          },
-        },
-      },
       particles: {
         color: {
           value: "#ffffff",
         },
         links: {
           color: "#ffffff",
-          distance: 150,
+          distance:100,
           enable: true,
-          opacity: 0.5,
-          width: 1,
+          opacity: .1,
+          width: 4.2,
+        },
+        collisions: {
+          enable: true,
         },
         move: {
-          direction: "none",
+          directions: "true",
           enable: true,
           outModes: {
             default: "bounce",
           },
-          random: false,
-          speed: 6,
+          random: true,
+          speed: 1,
           straight: false,
         },
         number: {
           density: {
             enable: true,
+            area: 800,
           },
           value: 80,
         },
@@ -70,14 +53,14 @@ const BackgroundAnimation = () => {
           type: "circle",
         },
         size: {
-          value: { min: 1, max: 5 },
+          value: { min: 1, max: 3 },
         },
       },
       detectRetina: true,
     }),
     []
   );
- 
+
   useEffect(() => {
     initParticlesEngine(async (engine) => {
       // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
@@ -98,56 +81,7 @@ const BackgroundAnimation = () => {
       id="tsparticles"
       init={init}
       loaded={particlesLoaded}
-      options={{
-        background: {
-          color: {
-            value: "#223a55",
-          },
-        },
-        fpsLimit: 120,
-        particles: {
-          color: {
-            value: "#ffffff",
-          },
-          links: {
-            color: "#ffffff",
-            distance: 150,
-            enable: true,
-            opacity: 0.5,
-            width: 0.2,
-          },
-          collisions: {
-            enable: true,
-          },
-          move: {
-            directions: "none",
-            enable: true,
-            outModes: {
-              default: "bounce",
-            },
-            random: false,
-            speed: 1,
-            straight: false,
-          },
-          number: {
-            density: {
-              enable: true,
-              area: 800,
-            },
-            value: 80,
-          },
-          opacity: {
-            value: 0.5,
-          },
-          shape: {
-            type: "circle",
-          },
-          size: {
-            value: { min: 1, max: 5 },
-          },
-        },
-        detectRetina: true,
-      }}
+      options={options}
     />
   );
 };
